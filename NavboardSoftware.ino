fe2ab98fa92427cc44ce992682cbbab1b201e128
Paul Nykiel <paul.nykiel@gmail.com>
2020-03-29 15:41:39 +0200

    Added Pitot Tube and SRF02

--- NavboardSoftware.ino
+++ NavboardSoftware.ino
@@ -17,10 +17,37 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 Adafruit_MPL3115A2 baro;
 
+void srf02_start() {
+  // step 1: instruct sensor to read echoes
+  Wire.beginTransmission(112); // transmit to device #112 (0x70)
+  // the address specified in the datasheet is 224 (0xE0)
+  // but i2c adressing uses the high 7 bits so it's 112
+  Wire.write(byte(0x00));      // sets register pointer to the command register (0x00)
+  Wire.write(byte(0x51));      // command sensor to measure in centimeters (0x51)
+  Wire.endTransmission();      // stop transmitting
+}
 
-// the setup function runs once when you press reset or power the board
+uint16_t srf02_read() {
+   // step 3: instruct sensor to return a particular echo reading
+  Wire.beginTransmission(112); // transmit to device #112
+  Wire.write(byte(0x02));      // sets register pointer to echo #1 register (0x02)
+  Wire.endTransmission();      // stop transmitting
+
+  // step 4: request reading from sensor
+  Wire.requestFrom(112, 2);    // request 2 bytes from slave device #112
+
+  // step 5: receive reading from sensor
+  uint16_t reading = 0;
+  if (2 <= Wire.available()) { // if two bytes were received
+    reading = Wire.read();  // receive high byte (overwrites previous reading)
+    reading = reading << 8;    // shift high byte to be high 8 bits
+    reading |= Wire.read(); // receive low byte as lower 8 bits
+  }
+  return reading;
+}
+
+
 void setup() {
-  // initialize digital pin 13 as an output.
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
 
@@ -28,7 +55,6 @@ void setup() {
   digitalWrite(RFM95_RST, HIGH);
 
   Serial.begin(9600);
-  Serial.write("Test\n");
 
   // manual reset
   digitalWrite(RFM95_RST, LOW);
@@ -50,12 +76,6 @@ void setup() {
   }
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
-  // you can set transmitter powers from 5 to 23 dBm:
-  //rf95.setTxPower(23, false);
   RH_RF95::ModemConfig modemConfig;
   modemConfig.reg_1d = 0b01110010;
   modemConfig.reg_1e = 0b01110100;
@@ -72,10 +92,27 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   static bool led = false;
-  
-  float altm = baro.getAltitude();
-  //Serial.print(altm); Serial.println("m");
 
+  // Altimeter
+  float altm = baro.getAltitude();
+  Serial.print(altm); Serial.println("m");
+
+  // Pitot Tube
+  uint16_t adc = analogRead(A0);
+  float outputVoltage = adc/1023.0F * 5; // Compensating for the voltage divider
+  
+  float pressurekPa = outputVoltage - 1;
+  Serial.print(outputVoltage); Serial.println("V");
+  Serial.print(pressurekPa); Serial.println("kPa");
+  float rho_air = 1.2041F;
+  float velocity = sqrt(2 * pressurekPa / rho_air);
+  Serial.print(velocity); Serial.println("m/s");
+
+  // SRF-02
+  srf02_start();
+  delay(70);
+  uint16_t distance = srf02_read();
+  Serial.print(distance); Serial.println("cm");
 
   // LoRa
   uint8_t buf[BUF_SIZE];
